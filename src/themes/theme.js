7d3825a2607208d470ed74449bb7f07dd4265a70
Otto Laitinen <otto.laitinen@hotmail.fi>
2020-10-10 23:05:13 +0300

    chore: add theme provider

---
+++ src/themes/theme.js
@@ -0,0 +1,78 @@
+const theme = {
+  colors: {
+    white: "#FFFFFF",
+    grayLight: "#C4C6CC",
+    gray: "#797C84",
+    grayDark: "#5C5E63",
+    grayDarker: "#222222",
+    black: "#000000",
+
+    primaryDark: "#9174CB",
+    primary: "#723BD9",
+    primaryLight: "#E7DCFC",
+    primaryLighter: "#EBECF2",
+
+    secondary: "#3A4055",
+
+    accentPositive: "#52C66C",
+    accentNegative: "#E23B62",
+
+    backgroundLight: "#FFFFFF",
+    backgroundDark: "#3B2B5A",
+    backgroundDarker: "#2B1B4B",
+  },
+  fonts: {
+    regular: "SourceSansPro-Regular",
+    semiBold: "SourceSansPro-SemiBold",
+    bold: "SourceSansPro-Bold",
+    black: "SourceSansPro-Black",
+  },
+  fontSizes: {
+    /** 12px */
+    tag: 12,
+    /** 14px */
+    small: 14,
+    /** 16px */
+    regular: 16,
+    /** 18px */
+    subtitle: 18,
+    /** 20px */
+    input: 20,
+    /** 24px */
+    loading: 24,
+    /** 26px */
+    title: 26,
+    /** 28px */
+    dialogTitle: 28,
+    /** 28px */
+    price: 36,
+    /** 40px */
+    large: 40,
+    /** 52px */
+    xLarge: 52,
+  },
+  /** 10px */
+  padding: 10,
+  /** 5px */
+  margin: 5,
+  /** 4px */
+  borderRadius: 4,
+  spacing: {
+    /** 1px */
+    xxSmall: 1,
+    /** 2px */
+    xSmall: 2,
+    /** 4px */
+    small: 4,
+    /** 8px */
+    regular: 8,
+    /** 16px */
+    large: 16,
+    /** 32px */
+    xLarge: 32,
+    /** 64px */
+    xxLarge: 64,
+  },
+}
+
+export default theme
